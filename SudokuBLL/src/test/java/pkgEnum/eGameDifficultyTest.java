559f4b5a85ac59a3a8bba8a1d175b15fd070d3d0
Kenny <Kenny@128.4.108.13>
2018-11-12 23:59:34 -0500

    Attempt at test cases

---
+++ SudokuBLL/src/test/java/pkgEnum/eGameDifficultyTest.java
@@ -0,0 +1,16 @@
+package pkgEnum;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class eGameDifficultyTest {
+
+	@Test
+	public void eGameDifficulty() {
+		eGameDifficulty newgame = new eGameDifficulty(100);
+	}
+	
+	
+
+}
